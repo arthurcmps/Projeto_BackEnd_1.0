8c3f1377b8c854f19fcec056f22e64cbd5b050fb
Arthur <arthur.felipe1993@gmail.com>
2024-09-28 03:47:48 -0300

    endereço

--- js/signup.js
+++ js/signup.js
@@ -319,23 +319,3 @@ const toggleMessage = (msg) => {
   fadeElement.classList.toggle("hide");
   messageElement.classList.toggle("hide");
 };
-
-// Close message modal
-closeButton.addEventListener("click", () => toggleMessage());
-
-// Save address
-addressForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  toggleLoader();
-
-  setTimeout(() => {
-    toggleLoader();
-
-    toggleMessage("Endereço salvo com sucesso!");
-
-    addressForm.reset();
-
-    toggleDisabled();
-  }, 1000);
-});
